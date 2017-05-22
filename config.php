5d3a70d7f6b5070de47d2559395982ddb8d6ed58
bigdata <bigdata@bigData>
2017-05-22 04:32:28 +0200

    FIXBUG tweets

--- config.php
+++ config.php
@@ -1,12 +1,10 @@
 <?php
 // Configure your Datumbox API Key. Get yours at http://www.datumbox.com/apikeys/view/
-define('46b39771164930ebc22ec8f198a6a1d9', '');
-// Configure your OAuth settings from your application at https://dev.twitter.com/apps
-define('Z0jz4btfOYMcnB4ududxrdT4s', '');
-define('LE2NSmShRInl6TjBXHsR5HzxUTxGYGW2eoNg4I5A00WDDKFlPU', '');
-// Configure authentication credentials.
-// you can generate your own access key from the link above
-define('453406916-AzyUBm8LeDNiD5BupGgdOxBaoYYzVLVO0YR86ejm', '');
-define('qfQkCCtlpWgvXtPgo16Dh2MOFlO4nvm077iwUt5qSojbn', '');
+define('datumbox_api_key', '859f0a4bfad7984c584e1e6f085b7052');
+// Mysql connexion
+define('DBHOST','127.0.0.1');
+define('DBUSER','root');
+define('DBPASS','root');
+define('DBNAME','bigdata');
 
 ?>
